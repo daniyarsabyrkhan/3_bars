f1e5c004cb91ec9f214d8063767086f9be4359a4
BI <bi@bi-VirtualBox>
2016-09-24 21:38:03 +0400

    вроде решил задачу

--- bars.py
+++ bars.py
@@ -1,21 +1,37 @@
 import json
+import operator
+import math
 
 
 def load_data(filepath):
-    pass
+    with open (filepath, "r") as bars:
+    	moscow_bars = json.load(bars)
+    	return moscow_bars
 
 
 def get_biggest_bar(data):
-    pass
+    biggest_bar = (max(data, key = lambda idata: idata["Cells"]["SeatsCount"]))
+    return biggest_bar
 
 
 def get_smallest_bar(data):
-    pass
+    smallest_bar = (min(data, key = lambda idata: idata["Cells"]["SeatsCount"]))
+    return smallest_bar
 
 
-def get_closest_bar(data, longitude, latitude):
-    pass
-
+def get_closest_bar(data, latitude, longitude):
+    def get_distance(bar):
+        lon, lat = bar['Cells']['geoData']['coordinates']
+        return ((lon - longitude) ** 2 + (lat - latitude) ** 2)
+    return min(data, key=get_distance)
+   
 
 if __name__ == '__main__':
-    pass
+    moscow_bars = load_data("bars.json")
+    latitude, longitude = map(float, input().split(', '))
+    biggest_bar = get_biggest_bar(moscow_bars)
+    smallest_bar = get_smallest_bar(moscow_bars)
+    closest_bar = get_closest_bar(moscow_bars, latitude, longitude)
+    print ("Самый большой бар: %s" %(biggest_bar["Cells"]["Name"]))
+    print ("Cамый маленький бар: %s" %(smallest_bar["Cells"]["Name"]))
+    print ("Cамый близкий бар: %s" %(closest_bar["Cells"]["Name"]))
